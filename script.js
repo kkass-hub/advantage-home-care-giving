b0b51a48b13244dbc0d51a6a0da5e5aeff2d8801
Kkass Tech <kkasssimo254@gmail.com>
2026-03-03 00:03:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ document.addEventListener('DOMContentLoaded', function() {
       const handler = PaystackPop.setup({
         key: 'pk_live_19caaea5b3f865073305c366d05254d3f50bcf1a', 
         email: 'customer@example.com', 
-        amount: 150000, // Amount in kobo (1500 KES)
-        currency: 'KES',
+        amount: 5000, // Amount in kobo (50 USD)
+        currency: 'USD',
         ref: ref,
         label: "Event Ticket Payment",
         callback: function(response) {
